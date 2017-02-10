e325a58c3a80d2fc62f69617c415502636e2679a
Tatsuya Nakano <admin@howdylikes.jp>
2017-02-10 12:40:02 +0900

    Fix resolve path

--- resolve.root_resolve.fallback_resolve.modulesDirectories/webpack.v1.config.js
+++ resolve.root_resolve.fallback_resolve.modulesDirectories/webpack.v1.config.js
@@ -2,11 +2,11 @@ const path = require('path');
 
 module.exports = {
   resolve: {
-    root: [path.resolve(__dirname, './src')],
+    root: [path.resolve(__dirname, 'src')],
     modulesDirectories: ['node_modules', 'my_modules'],
-    fallback: [path.resolve(__dirname, './src/fallback')],
+    fallback: [path.resolve(__dirname, 'src/fallback')],
   },
-  entry: path.resolve(__dirname, './src/app.js'),
+  entry: path.resolve(__dirname, 'src/app.js'),
   output: {
     filename: 'bundle.v1.js'
   },
--- resolve.root_resolve.fallback_resolve.modulesDirectories/webpack.v2.config.js
+++ resolve.root_resolve.fallback_resolve.modulesDirectories/webpack.v2.config.js
@@ -3,12 +3,12 @@ const path = require('path');
 module.exports = {
   resolve: {
     modules: [
-      path.resolve(__dirname, './src'),
+      path.resolve(__dirname, 'src'),
       'node_modules', 'my_modules',
-      path.resolve(__dirname, './src/fallback')
+      path.resolve(__dirname, 'src/fallback')
     ],
   },
-  entry: './src/app.js',
+  entry: path.resolve(__dirname, 'src/app.js'),
   output: {
     filename: 'bundle.v2.js'
   },
