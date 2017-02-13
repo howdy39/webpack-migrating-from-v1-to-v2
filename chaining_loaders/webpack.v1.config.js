f08ec553e3a550206ec1fef85ea88ac2ecbdae4f
Tatsuya Nakano <admin@howdylikes.jp>
2017-02-13 21:58:30 +0900

    Add Chaining loaders

---
+++ chaining_loaders/webpack.v1.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+module.exports = {
+  entry: path.resolve(__dirname, 'src/app.js'),
+  output: {
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader'
+      }
+    ]
+  }
+};
